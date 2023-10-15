710636f3267c57800d5bf039e44999bcf2f8107e
Angelo Marano <amarano@gmail.com>
2023-10-14 21:02:00 -0400

    initial commit

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,16 +4,8 @@ import Counter from "~/components/Counter";
 export default function Home() {
   return (
     <main>
-      <Title>Hello World</Title>
-      <h1>Hello world!</h1>
-      <Counter />
-      <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
-        </a>{" "}
-        to learn how to build SolidStart apps.
-      </p>
+      <Title>Ada Marano</Title>
+      <h1>Ada Marano's website is coming soon!</h1>
     </main>
   );
 }
